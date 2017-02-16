f4dede3f2a57c1d918b7ece9fb49b1056b88424e
Ignacio Anaya <ignacio.anaya89@gmail.com>
2017-02-16 13:31:43 -0300

    Adding content

---
+++ Sample.vue
@@ -0,0 +1,28 @@
+<template lang="pug">
+  div.box
+    p {{ msg }}
+    child-component
+</template>
+
+<script>
+import ChildComponent from './child-component.vue';
+
+export default {
+  components: { ChildComponent },
+
+  data() {
+    return: {
+      msg: 'Hello Vue.js!'
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .box {
+    width: 30%;
+    p {
+      color: red;
+    }
+  }
+</style>
